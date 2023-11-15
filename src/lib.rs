f83c3a838692fa5b17808d28d4ef223ad5cafe02
Mate Andras Magyar <mate.magyar@ukatemi.com>
2023-11-15 10:59:35 +0100

    Use c_char instead of u8

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::ffi::c_void;
+use std::ffi::{c_char, c_void};
 
 pub type tlsh_t = *mut c_void;
 
@@ -8,8 +8,8 @@ extern "C" {
     pub fn tlsh_update(tlsh: tlsh_t, data: *const u8, len: usize);
     pub fn tlsh_final(tlsh: tlsh_t);
     pub fn tlsh_reset(tlsh: tlsh_t);
-    pub fn tlsh_getHash(tlsh: tlsh_t) -> *const u8;
-    pub fn tlsh_getHashBuf(tlsh: tlsh_t, output: *mut u8, len: usize);
+    pub fn tlsh_getHash(tlsh: tlsh_t) -> *const c_char;
+    pub fn tlsh_getHashBuf(tlsh: tlsh_t, output: *mut c_char, len: usize);
     pub fn tlsh_totalDiff(tlsh: tlsh_t, other: tlsh_t) -> i32;
-    pub fn tlsh_fromTlshStr(tlsh: tlsh_t, str: *const u8) -> i32;
+    pub fn tlsh_fromTlshStr(tlsh: tlsh_t, str: *const c_char) -> i32;
 }
